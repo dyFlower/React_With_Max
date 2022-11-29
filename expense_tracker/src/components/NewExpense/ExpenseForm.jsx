adf86835393e2abe5944051b79095b64833d2191
Doyoung.Kim <dyflower@nate.com>
2022-11-30 02:54:45 +0900

    section_5

--- expense_tracker/src/components/NewExpense/ExpenseForm.jsx
+++ expense_tracker/src/components/NewExpense/ExpenseForm.jsx
@@ -59,7 +59,11 @@ const ExpenseForm = (props) => {
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
+          <input
+            type='text'
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
@@ -83,6 +87,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
