5ce3559f4c60745ad7d53335cf6f003784ccea3f
Doyoung.Kim <dyflower@nate.com>
2022-12-11 02:50:08 +0900

    section19_리덕스 심화

---
+++ redux_deep/src/components/Cart/CartButton.js
@@ -0,0 +1,20 @@
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/uiSlice';
+import classes from './CartButton.module.css';
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const toogleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
+  return (
+    <button className={classes.button} onClick={toogleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
